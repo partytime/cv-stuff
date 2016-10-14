c697606a44b278a009f1492bbbf121de16cdd81f
Bryce Evans <bryce-e@moving-picture.com>
2016-10-14 14:47:38 -0700

    successful detection

--- cat-detector/cat-detector.py
+++ cat-detector/cat-detector.py
@@ -19,9 +19,9 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # load the cat detector Haar cascade, then detect cat faces
 # in the input image
 detector = cv2.CascadeClassifier(args["cascade"])
-print detector
 rectList = []
 why = numpy.arange(1.1,1.5,0.1)
+# this is computationally expensive, but i want to try lots of vars
 for mn in xrange(1,10,):
     print "trying minNeighbor", mn
     for sf in why:
@@ -33,14 +33,12 @@ for mn in xrange(1,10,):
             rectList.append(rects)
 
 print rectList
+
 #draw over the kitty faces
 for r in rectList:
     print 'trying', r
-    #for (i, (x, y, w, h)) in enumerate(rects):
     for (x, y, w, h) in r:
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        #cv2.putText(image, "Cat #{}".format(1 + 1), (x, y - 10),
-            #cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
 
         #show em
         cv2.imshow("Cat faces", image)
